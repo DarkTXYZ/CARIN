1a9ceb4ce41e8c14c42ea49c22876beadbcbaa10
DarkTXYZ <pawaretmontfort@gmail.com>
2022-02-12 23:55:16 +0700

    parseExecutable implementation

--- Parser Code/src/GeneticEvaluator.java
+++ Parser Code/src/GeneticEvaluator.java
@@ -8,41 +8,141 @@ interface Evaluator {
      * @return the executable program of input unit
      * @throws SyntaxErrorException if an unidentified token type has been found
      */
-    Executable evaluate(Unit unit) throws SyntaxErrorException;
+    Executable evaluate(Unit unit) throws SyntaxErrorException, TokenizeErrorException;
 }
 
 public class GeneticEvaluator implements Evaluator {
 
     // Singleton
     private static GeneticEvaluator instance;
-    private GeneticEvaluator() {
 
+    private GeneticEvaluator() {
+        this.tkz = Tokenizer.getInstance();
     }
 
     public static GeneticEvaluator getInstance() {
-        if(GeneticEvaluator.instance == null)
+        if (GeneticEvaluator.instance == null)
             GeneticEvaluator.instance = new GeneticEvaluator();
         return GeneticEvaluator.instance;
     }
 
-    private Tokenizer tkz;
-    private Map<String, Integer> bindings;
-    private Unit unit;
-
+    // GeneticEvaluator field
     private final String[] reservedWordList = {"antibody", "down", "downleft", "downright",
             "else", "if", "left", "move", "nearby", "right",
             "shoot", "then", "up", "upleft", "upright", "virus", "while"};
     private final Set<String> reservedWords = new HashSet<>(List.of(reservedWordList));
-
     private final String[] directionWordList = {"up", "left", "right", "down",
             "upleft", "upright", "downleft", "downright"};
     private final Set<String> directionWords = new HashSet<>(List.of(directionWordList));
 
+    private final Tokenizer tkz;
+    private Map<String, Integer> bindings;
+    private Unit unit;
 
     @Override
-    public Executable evaluate(Unit unit) {
-        return null;
+    public Executable evaluate(Unit unit) throws TokenizeErrorException, SyntaxErrorException {
+        tkz.initialize(unit.getGeneticCode());
+        this.bindings = unit.getBindings();
+        this.unit = unit;
+
+        return parseProgram();
     }
+
+    private Executable parseProgram() throws SyntaxErrorException, TokenizeErrorException {
+        Program program = new Program();
+
+        program.addStatement(parseStatement());
+        while (tkz.hasNext())
+            program.addStatement(parseStatement());
+
+        return program;
+    }
+
+    private Executable parseStatement() throws SyntaxErrorException, TokenizeErrorException {
+        if (tkz.peek("{"))
+            return parseBlockStatement();
+        else if (tkz.peek("if"))
+            return parseIfStatement();
+        else if (tkz.peek("while"))
+            return parseWhileStatement();
+        else
+            return parseCommand();
+    }
+
+    private Executable parseCommand() throws SyntaxErrorException, TokenizeErrorException {
+        if (tkz.peek("move") || tkz.peek("shoot"))
+            return parseActionCommand();
+        else
+            return parseAssignmentStatement();
+    }
+
+    private Executable parseAssignmentStatement() throws SyntaxErrorException, TokenizeErrorException {
+        String identifier = tkz.consume();
+        tkz.consume("=");
+        Evaluable expression = parseExpression();
+        if (reservedWords.contains(identifier))
+            throw new SyntaxErrorException("Use reserved word as identifier: " + identifier);
+        return new AssignmentStatement(identifier, expression, bindings);
+    }
+
+    private Executable parseActionCommand() throws SyntaxErrorException, TokenizeErrorException {
+        if (tkz.peek("shoot"))
+            return parseAttackCommand();
+        else if (tkz.peek("move"))
+            return parseMoveCommand();
+        else
+            return null;
+    }
+
+    private Executable parseMoveCommand() throws SyntaxErrorException, TokenizeErrorException {
+        tkz.consume("move");
+        String direction = tkz.consume();
+        if (!directionWords.contains(direction))
+            throw new SyntaxErrorException("Missing direction word");
+        return new MoveCommand(unit, direction);
+    }
+
+    private Executable parseAttackCommand() throws SyntaxErrorException, TokenizeErrorException {
+        tkz.consume("shoot");
+        String direction = tkz.consume();
+        if (!directionWords.contains(direction))
+            throw new SyntaxErrorException("Missing direction word");
+        return new AttackCommand(unit, direction);
+    }
+
+    private Executable parseBlockStatement() throws SyntaxErrorException, TokenizeErrorException {
+        tkz.consume("{");
+        BlockStatement block = new BlockStatement();
+        while (!tkz.peek("}"))
+            block.addStatement(parseStatement());
+        tkz.consume("}");
+
+        return block;
+    }
+
+    private Executable parseIfStatement() throws SyntaxErrorException, TokenizeErrorException {
+        tkz.consume("if");
+        tkz.consume("(");
+        Evaluable expression = parseExpression();
+        tkz.consume(")");
+        tkz.consume("then");
+        Executable trueStatement = parseStatement();
+        tkz.consume("else");
+        Executable falseStatement = parseStatement();
+
+        return new IfStatement(trueStatement, falseStatement, expression);
+    }
+
+    private Executable parseWhileStatement() throws SyntaxErrorException, TokenizeErrorException {
+        tkz.consume("while");
+        tkz.consume("(");
+        Evaluable expression = parseExpression();
+        tkz.consume(")");
+        Executable statement = parseStatement();
+
+        return new WhileStatement(statement, expression);
+    }
+
 
     private Evaluable parseExpression() throws SyntaxErrorException, TokenizeErrorException {
         Evaluable term = parseTerm();
