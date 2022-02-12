ce1ba0ecf125e76741171430107e90bf809c7b34
DarkTXYZ <pawaretmontfort@gmail.com>
2022-02-12 23:55:38 +0700

    testing some unit with GeneticEvaluator

--- Parser Code/src/TestMain.java
+++ Parser Code/src/TestMain.java
@@ -1,8 +1,12 @@
 public class TestMain {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SyntaxErrorException, TokenizeErrorException {
         GeneticEvaluator g = GeneticEvaluator.getInstance();
-        System.out.println(g);
-        g = GeneticEvaluator.getInstance();
-        System.out.println(g);
+        VirusDummy v = new VirusDummy("a=5 while(a) {move up a=a-1}");
+        VirusDummy w = new VirusDummy("b=8 while(b-5) {move down move left b = b-1}");
+        v.setProgram(g.evaluate(v));
+        w.setProgram(g.evaluate(w));
+
+        v.execute();
+        w.execute();
     }
 }
