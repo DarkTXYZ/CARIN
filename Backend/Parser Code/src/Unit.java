8b0791ba1b4fbf21432f4edc81b089305c76b3ed
DarkTXYZ <pawaretmontfort@gmail.com>
2022-02-14 01:53:16 +0700

    add Javadoc for unit interface

--- Backend/Parser Code/src/Unit.java
+++ Backend/Parser Code/src/Unit.java
@@ -4,19 +4,34 @@ public interface Unit {
 
     /**
      * Move unit to the input direction one space
+     *
      * @param direction the direction that the unit move
      */
     void move(String direction);
 
     /**
      * Make the unit shoot to the input direction
+     *
      * @param direction the direction that the unit shoot
      */
     void shoot(String direction);
 
+    /**
+     * Return the genetic code of the unit
+     *
+     * @return the unit's genetic code
+     */
     String getGeneticCode();
 
-    Map<String,Integer> getBindings();
+    /**
+     * Return the binding of the unit
+     *
+     * @return bindings
+     */
+    Map<String, Integer> getBindings();
 
+    /**
+     * Execute the program of the unit
+     */
     void execute();
 }
