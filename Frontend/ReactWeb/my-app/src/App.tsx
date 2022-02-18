0de6ba585d91f84db2e3a9cfe0a9e9a22464d7d6
DarkTXYZ <pawaretmontfort@gmail.com>
2022-02-19 00:35:46 +0700

    old web files

--- Frontend/ReactWeb/my-app/src/App.tsx
+++ Frontend/ReactWeb/my-app/src/App.tsx
@@ -7,25 +7,40 @@ type Post = {
   id: number,
   name: string,
   nums: Array<number>
-  mapped : any
+  mapped: any,
+  role: string
 }
 
 const App = () => {
 
-  const [posts, setPosts] = useState([])
-  
+  const [posts, setPosts] = useState<Post[]>([])
+
   const getPosts = async () => {
     try {
-      const userPosts = await axios.get("http://localhost:8080/employees")
-      .then(response => {
+
+      await axios({
+        method : 'put',
+        url: 'http://localhost:8080/employees/1',
+        data: {
+          name: "Fredfffff" ,
+          nums: [1,3,3] ,
+          role: "gardener",
+          mapped: {
+            "ggg" : 1222,
+            "lll" : 345
+          }
+        }
+      })
+
+      await axios({
+        method: 'get',
+        url: 'http://localhost:8080/employees',
+        responseType: 'stream'
+      }).then(response => {
         setPosts(response.data);
         console.log(response);
       })
-      // await axios.post(link, {
-      //   name: 'Fred Flintstone',
-      //   role: 'pilot',
-      //   nums: [1,2,3,4]
-      // })
+
 
     } catch (err: any) {
       console.error(err.message);
@@ -38,18 +53,18 @@ const App = () => {
       getPosts()
     }, 5000)
     return () => clearInterval(interval)
-  }, [])  // includes empty dependency array
+  })  // includes empty dependency array
 
   return (
     <div>
       <h1>useEffect</h1>
       <ul>
         {posts.map((post: Post) => (
-          <li key={post.id}>{post.name} {post.nums} {post.mapped.sss} {post.mapped.abc}</li>
+          <li key={post.id}>{post.name} {post.nums} {post.role} </li>
         ))}
       </ul>
     </div>
   );
-  
+
 }
 export default App;
